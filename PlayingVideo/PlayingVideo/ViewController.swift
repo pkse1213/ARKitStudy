c81afdc3ed5521902728d89b165d52300d5c2223
박세은 <pkse1213@naver.com>
2018-11-26 02:35:39 +0900

    Playing Video 완료

--- PlayingVideo/PlayingVideo/ViewController.swift
+++ PlayingVideo/PlayingVideo/ViewController.swift
@@ -16,21 +16,51 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
-        // Create a new scene
         let scene = SCNScene()
-        
-        // Set the scene to the view
         sceneView.scene = scene
+        
+        registerGestureRecognizers()
     }
     
-    private func  
+    private func registerGestureRecognizers() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func tapped(recognizer: UITapGestureRecognizer ) {
+        guard let currentFrame = self.sceneView.session.currentFrame  else {return}
+        
+        // 비디오 node 생성
+        let videoNode = SKVideoNode(fileNamed: "big_buck_bunny.mp4")
+        videoNode.play()
+        
+        // Sprite Scene
+        let skScene = SKScene(size: CGSize(width: 640, height: 480))
+        skScene.addChild(videoNode)
+        
+        // Sprite Scene의 가운데에 videoNode 올리기
+        videoNode.position = CGPoint(x: skScene.size.width/2, y: skScene.size.height/2)
+        videoNode.size = skScene.size
+        
+        // 어떻게 Sprite Scene을 sceneview에 올릴 수 있을까???
+        let tvPlane = SCNPlane(width: 1.0, height: 0.75)
+        tvPlane.firstMaterial?.diffuse.contents = skScene // 이렇게 하면 됨!!!
+        // plane의 반대 면도 볼 수 있음
+        tvPlane.firstMaterial?.isDoubleSided = true
+        
+        let tvPlaneNode = SCNNode(geometry: tvPlane)
+        
+        var translation = matrix_identity_float4x4
+        // 나와 더 멀리
+        translation.columns.3.z = -1.0
+       
+        tvPlaneNode.simdTransform = matrix_multiply(currentFrame.camera.transform, translation)
+        tvPlaneNode.eulerAngles = SCNVector3(Double.pi, 0, 0)
+        
+        self.sceneView.scene.rootNode.addChildNode(tvPlaneNode)
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
